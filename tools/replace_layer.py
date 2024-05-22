ddf8c953cd31398571383c70128a19bb670d02d4
OmarMoMorgan <s-omar.morgan@zewailcity.edu.eg>
2024-05-22 15:49:05 +0300

    fixed it once more

--- tools/replace_layer.py
+++ tools/replace_layer.py
@@ -11,9 +11,10 @@ def replace_fc_layers(module, in_features, out_features,NewModule):
             replace_fc_layers(child, in_features, out_features)
 
 
-def replace_specific_layers(module, layer_names, in_features, out_features,NewModule):
+def replace_specific_layers(module, layer_names, n_features,NewModule):
     for name, child in module.named_children():
         if name in layer_names and isinstance(child, nn.Linear):
-            setattr(module, name, NewModule(in_features, out_features))
+            setattr(module, name, NewModule(n_features))
         else:
-            replace_specific_layers(child, layer_names, in_features, out_features,NewModule)
+            replace_specific_layers(child, layer_names, n_features,NewModule)
+#this shoudl be fixed later this is a quick fix for now
